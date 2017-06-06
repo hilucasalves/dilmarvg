e025bb9b076286ca67eb096a27e426a101c81edb
JhonnyThorne <jonathansilvaviana@gmail.com>
2017-06-06 12:22:52 -0300

    Criação do MenuPrincipal
    Criação teste do Menu principal do game.

---
+++ Assets/_Complete-Game/Scripts/Menu.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+function MenuPrincipal()
+{
+	Application.LoadLevel("Menu");
+}
+
+function IniciarJogo()
+{
+	Application.LoadLevel("_Complete-Game");
+}
+//function Continue()
+//{
+    
+//}
+function Creditos()
+{
+    Application.LoadLevel("creditos");
+}
+//function Options()
+//{
+    
+//}
+function Sair()
+{
+  Application.Quit();
+
+}
